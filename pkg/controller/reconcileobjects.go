42a01fd7fffa63a61437b572471082cb92ff7e39
Nikhil Thomas <nikthoma@redhat.com>
2022-10-12 16:34:46 -0400

    Improve apiResource listing mechansim for deciding resources to be validated
    Add a check to ensure that a Resource supported by the server is registered
    in the client-go scheme
    
    Add a mechanism to make sure that the version of the resource is chosen based on
    what is supported by client scheme when server supports more than one version
    
    Signed-off-by: Nikhil Thomas <nikthoma@redhat.com>

--- pkg/controller/reconcileobjects.go
+++ pkg/controller/reconcileobjects.go
@@ -1,40 +1,85 @@
 package controller
 
 import (
+	"strings"
+
 	"golang.stackrox.io/kube-linter/pkg/objectkinds"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 )
 
-func reconcileResourceList(client discovery.DiscoveryInterface) ([]metav1.APIResource, error) {
-	apiResourceLists, err := client.ServerPreferredResources()
+func reconcileResourceList(client discovery.DiscoveryInterface, scheme *runtime.Scheme) ([]metav1.APIResource, error) {
+	apiResources := []metav1.APIResource{}
+	apiResourceSet := map[schema.GroupKind]metav1.APIResource{}
+
+	_, apiResourceLists, err := client.ServerGroupsAndResources()
 	if err != nil {
 		return nil, err
 	}
-	apiResources := []metav1.APIResource{}
-	for _, apiResourceGroup := range apiResourceLists {
-		gv, err := schema.ParseGroupVersion(apiResourceGroup.GroupVersion)
+
+	for _, apiResourceList := range apiResourceLists {
+		gv, err := schema.ParseGroupVersion(apiResourceList.GroupVersion)
 		if err != nil {
 			return nil, err
 		}
-		for _, apiResource := range apiResourceGroup.APIResources {
-
-			apiResource.Version = gv.Version
+		for _, apiResource := range apiResourceList.APIResources {
+			// skip sub resources (pods/scale, deployment/status...)
+			if isSubResource(&apiResource) {
+				continue
+			}
 			apiResource.Group = gv.Group
+			apiResource.Version = gv.Version
 
 			canValidate, err := isRegisteredKubeLinterKind(apiResource)
 			if err != nil {
 				return nil, err
 			}
-
 			if !canValidate {
 				continue
 			}
-			apiResources = append(apiResources, apiResource)
+
+			if !isRegisteredInScheme(&apiResource, scheme) {
+				continue
+			}
+
+			gk := schema.GroupKind{
+				Group: apiResource.Group,
+				Kind:  apiResource.Kind,
+			}
+			existing, ok := apiResourceSet[gk]
+			if !ok {
+				apiResourceSet[gk] = apiResource
+				continue
+			}
+			priorityGV := getPriorityVersion(existing.Group, existing.Version, apiResource.Version, scheme)
+			existing.Version = priorityGV
+			apiResourceSet[gk] = existing
 		}
 	}
+	for _, v := range apiResourceSet {
+		apiResources = append(apiResources, v)
+	}
 	return apiResources, nil
+}
+
+func getPriorityVersion(group, existingVer, currentVer string, scheme *runtime.Scheme) string {
+	gv := scheme.PrioritizedVersionsAllGroups()
+	for _, v := range gv {
+		if v.Version == existingVer {
+			return existingVer
+		}
+		if v.Version == currentVer {
+			return currentVer
+		}
+	}
+	return existingVer
+}
+
+// isSubResource returns true if the apiResource.Name has a "/" in it eg: pod/status
+func isSubResource(apiResource *metav1.APIResource) bool {
+	return strings.Contains(apiResource.Name, "/")
 }
 
 func isRegisteredKubeLinterKind(rsrc metav1.APIResource) (bool, error) {
@@ -70,4 +115,13 @@ func gvkFromMetav1APIResource(rsc metav1.APIResource) schema.GroupVersionKind {
 		Version: rsc.Version,
 		Kind:    rsc.Kind,
 	}
+}
+
+func isRegisteredInScheme(apiResource *metav1.APIResource, scheme *runtime.Scheme) bool {
+	gvk := schema.GroupVersionKind{
+		Group:   apiResource.Group,
+		Version: apiResource.Version,
+		Kind:    apiResource.Kind,
+	}
+	return scheme.Recognizes(gvk)
 }
